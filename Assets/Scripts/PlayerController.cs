37861937766b38225f61667e3a96b95d2674d802
ElMexicano28 <theofficialvictorgarcia@gmail.com>
2026-04-17 15:27:43 -0400

    Fight plane submission Week 12
    added sprites, animations, coin and health pickups, score and lives

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int lives;
+    private float speed;
+
+    private GameManager gameManager;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    public GameObject bulletPrefab;
+    public GameObject bigBulletPrefab;
+    public GameObject explosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        lives = 3;
+        speed = 5.0f;
+        gameManager.ChangeLivesText(lives);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Shooting();
+    }
+
+    public void LoseALife()
+    {
+        //lives = lives - 1;
+        //lives -= 1;
+        lives--;
+        gameManager.ChangeLivesText(lives);
+        if (lives == 0)
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void GainALife()
+    {
+        if(lives == 3)
+        {
+            gameManager.AddScore(1);
+        }
+        else
+        {
+            lives++;
+            gameManager.ChangeLivesText(lives);
+        }
+    }
+
+    void Shooting()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(bigBulletPrefab, transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+        }
+    }
+
+    void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * speed);
+
+        float horizontalScreenSize = gameManager.horizontalScreenSize;
+        float verticalScreenSize = gameManager.verticalScreenSize;
+
+        if (transform.position.x <= -horizontalScreenSize || transform.position.x > horizontalScreenSize)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+        }
+
+        if (transform.position.y <= -verticalScreenSize)
+        {
+            transform.position = new Vector3(transform.position.x, -verticalScreenSize, 0);
+        }
+
+        //Player verticalboundaries
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+    }
+}
